341b110be911f0bfce40a76ed0579807c6784309
Juri Ryzhkov <plktmn@gmail.com>
2023-11-30 13:13:08 +0100

    add Lesson 21

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@
 import './App.css';
 // import Lesson20 from './lessons/Lesson20';
 // import Consutation20 from './consultations/Consultation20';
-import Homework20 from './homeworks/Homework20';
+// import Homework20 from './homeworks/Homework20';
+import Lesson21 from './lessons/Lesson21';
 
 function App() {
   return (
     <div className="lesson20-wrapper">
      {/* <Lesson20 /> */}
      {/* <Consutation20 /> */}
-     <Homework20 />
+     {/* <Homework20 /> */}
+     <Lesson21 />
+
     </div>
   );
 }
